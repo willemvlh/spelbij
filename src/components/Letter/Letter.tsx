4abaca98a7366d8defb1702baf2c9eb5bf29f406
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2021-04-17 20:24:44 +0200

    add pushdown style to letters

--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -1,6 +1,7 @@
 import { connect } from "react-redux"
 import styles from "./Letter.module.css"
 import { AddLetterAction } from "../../store/Types"
+import { useState} from "react";
 
 const mapDispatchToProps = (dispatch: ((action: AddLetterAction) => any)) => {
     return {
@@ -8,7 +9,12 @@ const mapDispatchToProps = (dispatch: ((action: AddLetterAction) => any)) => {
     }
 }
 
-const LetterComponent = ({ letter, isMiddle, addLetter }: any) =>
-    <div onClick={() => addLetter(letter)} className={isMiddle ? `${styles.cell} ${styles.middle} `: styles.cell}>{letter}</div>
+const LetterComponent = ({ letter, isMiddle, addLetter }: any) => {
+    const [showFlash, setShowFlash] = useState(false);
+    const onMouseUp = () => setShowFlash(false)
+
+    return <div onMouseDown={() => setShowFlash(true)} onMouseLeave={onMouseUp} onMouseUp={onMouseUp} onClick={() => addLetter(letter)}
+         className={`${styles.cell} ${(showFlash ? styles.flash : "")} ${isMiddle ? ` ${styles.middle} ` : styles.cell}`}>{letter}</div>;
+}
 
 export default connect(null, mapDispatchToProps)(LetterComponent)
