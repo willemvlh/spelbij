4e7f6c13c4dd9ba15aa96c8b60b2d5536cd444fb
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2021-04-24 21:48:25 +0200

    Make modal scrollable

--- src/components/Header/Modal.tsx
+++ src/components/Header/Modal.tsx
@@ -1,13 +1,25 @@
-import styles from "./Header.module.css";
 import React from "react";
 import Modal from "react-modal";
 
-const SBModal:React.FC<{isOpen: boolean, onRequestClose: () => void, shouldCloseOnOverlayClick?: boolean}> =
+const SBModal: React.FC<{ isOpen: boolean, onRequestClose: () => void, shouldCloseOnOverlayClick?: boolean }> =
     ({shouldCloseOnOverlayClick = true, isOpen, onRequestClose, children}) => {
-    return <Modal shouldCloseOnOverlayClick={shouldCloseOnOverlayClick} className={styles.modal} ariaHideApp={false} onRequestClose={onRequestClose}
-                  isOpen={isOpen} shouldCloseOnEsc={true}>
-        {children}
-    </Modal>
-}
+        return <Modal
+            style={{
+                content: {
+                    width: "300px",
+                    maxWidth: "95%",
+                    border: "solid 1px #aaa",
+                    background: "white",
+                    padding: "20px",
+                    boxShadow: "3px 3px 3px grey",
+                    margin: 'auto'
+                }
+            }}
+            shouldCloseOnOverlayClick={shouldCloseOnOverlayClick} ariaHideApp={false}
+            onRequestClose={onRequestClose}
+            isOpen={isOpen} shouldCloseOnEsc={true}>
+            {children}
+        </Modal>
+    }
 
 export default SBModal
