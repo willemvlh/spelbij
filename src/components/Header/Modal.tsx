23cd78379fcb06d52bb0a5ba730daa838c88cbd0
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2021-04-25 18:25:50 +0200

    fix up dialog size, add close button

--- src/components/Header/Modal.tsx
+++ src/components/Header/Modal.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import Modal from "react-modal";
+import styles from "./Modal.module.css"
+import {CloseOutlined} from "@material-ui/icons";
 
 const SBModal: React.FC<{ isOpen: boolean, onRequestClose: () => void, shouldCloseOnOverlayClick?: boolean }> =
     ({shouldCloseOnOverlayClick = true, isOpen, onRequestClose, children}) => {
         return <Modal
             style={{
                 content: {
-                    width: "300px",
-                    maxWidth: "95%",
+                    fontFamily: "Noto Serif, serif",
+                    maxWidth: "320px",
                     border: "solid 1px #aaa",
                     background: "white",
-                    padding: "20px",
+                    padding: "18px",
                     boxShadow: "3px 3px 3px grey",
-                    margin: 'auto'
+                    margin: "auto",
+                    bottom: "auto"
                 }
             }}
             shouldCloseOnOverlayClick={shouldCloseOnOverlayClick} ariaHideApp={false}
             onRequestClose={onRequestClose}
             isOpen={isOpen} shouldCloseOnEsc={true}>
-            {children}
+            <div className={styles.content}>
+                {shouldCloseOnOverlayClick && <div style={{textAlign: "right"}} onClick={onRequestClose}><CloseOutlined/></div>}
+                {children}
+            </div>
+
         </Modal>
     }
 
--- src/components/Header/Modals/HelpModal.tsx
+++ src/components/Header/Modals/HelpModal.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 const HelpModal:React.FC<{isOpen: boolean, onRequestClose: () => void}> = (props) => {
     return <SBModal {...props}>
-        <h1>Spelbij</h1>
         <ul>
             <li>Woorden moeten minstens 4 letters tellen.</li>
             <li>Woorden moeten de middenste letter bevatten.</li>
