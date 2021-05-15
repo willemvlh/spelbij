18924fc0f2f3da335feeb4556768a70c9ad2c835
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2021-05-15 16:24:31 +0200

    set max score to 80% of possible score to make progress bar more hopeful to look at

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -6,7 +6,8 @@ type Props = {
     totalScore: number
 }
 const ProgressBar: React.FC<Props> = ({score, totalScore}) => {
-    const calculateWidth = () => (score / totalScore) * 100
+    const weightedTotalScore = totalScore * 0.8; //assume nobody will ever reach 80%
+    const calculateWidth = () => (score / weightedTotalScore) * 100
     return <div className={styles.progress} style={{width: calculateWidth() + "%"}}/>
 }
 
