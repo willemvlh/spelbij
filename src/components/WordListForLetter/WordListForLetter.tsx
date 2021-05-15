e933c2b8ab5660d163b49c829010b9e004523c94
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2021-05-15 16:18:21 +0200

    sort found words in list

--- src/components/WordListForLetter/WordListForLetter.tsx
+++ src/components/WordListForLetter/WordListForLetter.tsx
@@ -2,20 +2,29 @@ import styles from "./WordListForLetter.module.css";
 import {CheckCircleOutline} from "@material-ui/icons"
 import React from "react";
 
-export function WordListForLetter(props: { letter: string, foundWords: string[], allWords: string[], displayMissedWords: boolean }) {
-    const wordsToShow = props.displayMissedWords ? props.allWords : props.foundWords
-    const calculateStyle = (word: string) => props.foundWords.includes(word) ? styles.foundWord : styles.missedWord
+type WordListForLetterProps = {
+    letter: string,
+    foundWords: string[],
+    allWords: string[],
+    displayMissedWords: boolean,
+    shouldSort?: boolean };
 
+export function WordListForLetter({allWords, displayMissedWords, foundWords, letter, shouldSort = true}: WordListForLetterProps) {
+    const wordsToShow = displayMissedWords ? allWords : foundWords;
+    const orderedWordsToShow = shouldSort ? wordsToShow.sort() : wordsToShow;
+    const calculateStyle = (word: string) => foundWords.includes(word) ? styles.foundWord : styles.missedWord
+    const isEveryWordFound = allWords.length > 0 && foundWords.length === allWords.length;
     return <div className={styles.foundWordsPart}>
         <div className={styles.header}>
-            <span className={styles.letter}>{props.letter}</span>
-            <span className={styles.letterCount}>({props.foundWords.length} / {props.allWords.length})</span>
-            {(props.allWords.length > 0 && props.foundWords.length === props.allWords.length) &&
-            <CheckCircleOutline htmlColor={"#2b9715fc"} fontSize={"small"} alignmentBaseline={"baseline"} viewBox={"-5 -5 32 32"}/>}
+            <span className={styles.letter}>{letter}</span>
+            <span className={styles.letterCount}>({foundWords.length} / {allWords.length})</span>
+            {isEveryWordFound &&
+            <CheckCircleOutline htmlColor={"#2b9715fc"} fontSize={"small"} alignmentBaseline={"baseline"}
+                                viewBox={"-5 -5 32 32"}/>}
         </div>
 
         <div className={styles.foundWordsContainer}>
-            {wordsToShow.map(word =>
+            {orderedWordsToShow.map(word =>
                 <div key={word} className={styles.word + " " + calculateStyle(word)}>{word}</div>
             )}
         </div>
